80334d2ff778c355251fa596d4d82f59d94e63dc
basithbahi <basithbahi@gmail.com>
2023-04-13 11:08:47 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register()
+    {
+        return view('auth/register');
+    }
+
+    public function registerSimpan(Request $request)
+    {
+        Validator::make($request->all(), [
+            'nik' => 'required',
+            'nama' => 'required',
+            'alamat' => 'required',
+            'ttl' => 'required',
+            'jk' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed'
+        ])->validate();
+
+        User::create([
+            'nik' => $request->nik,
+            'nama' => $request->nama,
+            'alamat' => $request->alamat,
+            'ttl' => $request->ttl,
+            'jk' => $request->jk,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'level' => 'Admin'
+        ]);
+
+        return redirect()->route('login');
+    }
+
+    public function login()
+    {
+        return view('auth/login');
+    }
+
+    public function loginAksi(Request $request)
+    {
+        Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
+        ])->validate();
+
+        if (!Auth::attempt($request->only('email', 'password'), $request->boolean('remember'))) {
+            throw ValidationException::withMessages([
+                'email' => trans('auth.failed')
+            ]);
+        }
+
+        $request->session()->regenerate();
+
+        return redirect()->route('dashboard');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        return redirect('/');
+    }
+}
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,78 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon">
+            <i class="fas fa-dryer"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">LAUNDRY</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-1">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('dashboard') }}">
+            <i class="fas fa-fw fa-home"></i>
+            <span>Dashboard</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('dashboard') }}">
+            <i class="fas fa-fw fa-user"></i>
+            <span>Admin</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('dashboard') }}">
+            <i class="fas fa-fw fa-users"></i>
+            <span>User</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('dashboard') }}">
+            <i class="fas fa-fw fa-costume"></i>
+            <span>Jenis Cucian</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('dashboard') }}">
+            <i class="fas fa-fw fa-trailer"></i>
+            <span>Gerbong</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('dashboard') }}">
+            <i class="fas fa-fw fa-couch"></i>
+            <span>Kursi</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('dashboard') }}">
+            <i class="fas fa-fw fa-calendar"></i>
+            <span>Jadwal Kereta Api</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('dashboard') }}">
+            <i class="fas fa-fw fa-route"></i>
+            <span>Rute</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('dashboard') }}">
+            <i class="fas fa-fw fa-building"></i>
+            <span>Stasiun</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('dashboard') }}">
+            <i class="fas fa-fw fa-history"></i>
+            <span>Transaksi</span></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('dashboard') }}">
+            <i class="fas fa-fw fa-wallet"></i>
+            <span>Metode Pembayatan</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+</ul>
