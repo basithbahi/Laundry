92163e4b73097787b19c6e55b9c1cf5f2d19356b
taristadwi <taristadwi60@gmail.com>
2023-05-27 22:26:49 -0700

    register dan login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
+use App\Traits\ApiResponse;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -12,11 +17,27 @@ class AuthController extends Controller
     public function register(RegisterRequest $request)
     {
         $validated = $request->validated();
-        $user = User::create([
-            'name' => $validated['name'],
+
+        $userData = [
+            'nik' => $validated['nik'],
+            'nama' => $validated['nama'],
+            'alamat' => $validated['alamat'],
+            'ttl' => $validated['ttl'],
+            'jk' => $validated['jk'],
             'email' => $validated['email'],
             'password' => Hash::make($validated['password']),
-        ]);
+            'level' => $validated['level'],
+        ];
+
+        if ($request->hasFile('foto_profil')) {
+            $fotoProfil = $request->file('foto_profil');
+            // Lakukan validasi atau manipulasi tambahan pada file gambar jika diperlukan
+            // Misalnya, memeriksa tipe file, ukuran file, dll.
+            $fotoPath = $fotoProfil->store('foto_profil', 'public');
+            $userData['foto_profil'] = $fotoPath;
+        }
+
+        $user = User::create($userData);
 
         $token = $user->createToken('auth_token')->plainTextToken;
         return $this->apiSuccess([
