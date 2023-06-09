cb95d162e18ef0a013f83e9cd99cdd2333aabaf9
taristadwi <taristadwi60@gmail.com>
2023-06-09 13:45:00 -0700

    api

--- routes/api.php
+++ routes/api.php
@@ -1,27 +1,20 @@
 <?php
-  
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-  
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\BlogController;
-  
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 */
-<<<<<<< HEAD
-  
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-     
 // Route::middleware('auth:sanctum')->group(function () {
 //     Route::posts('/logout', [AuthController::class, 'logout']);
 //     Route::apiResource('/todos', TodoController::class);
 //     return $request->user();
 // });
-=======
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::posts('/logout', [AuthController::class, 'logout']);
@@ -31,4 +24,4 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
->>>>>>> d7969669304d502c7c4188283e2084b60ece646c
+?>
